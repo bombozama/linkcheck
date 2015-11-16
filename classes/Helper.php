2cfb8ceb021bcf9ba57d65e3d1fc03745365599e
Gonzalo Henríquez <dev@barlovento.io>
2015-11-16 16:53:27 -0300

    Nothing

--- classes/Helper.php
+++ classes/Helper.php
@@ -14,15 +14,17 @@ class Helper
         $fp = fopen($pathToFile, 'r');
         $class = $namespace = $buffer = '';
         $i = 0;
-        while(!$class){
-            if(feof($fp))
+        while (!$class) {
+            if (feof($fp))
                 break;
+
             $buffer.= fread($fp, 512);
             $tokens = token_get_all($buffer);
 
-            if(strpos($buffer, '{') === false)
+            if (strpos($buffer, '{') === false)
                 continue;
-            for(; $i < count($tokens); $i++) {
+
+            for (; $i < count($tokens); $i++) {
                 if($tokens[$i][0] === T_NAMESPACE)
                     for($j = $i+1; $j < count($tokens); $j++)
                         if($tokens[$j][0] === T_STRING)
